5e93a634dbbdd0e8a2993f1b9ff6889e4ba57a2a
qw3rty-dev <rishabh120805@gmail.com>
2026-03-13 23:16:02 +0530

    Refactor number_between_worksheet.py for clarity
    Refactor input handling and score tracking in the worksheet function. Improve user prompts and feedback for incorrect answers.

--- number_between_worksheet.py
+++ number_between_worksheet.py
@@ -1,46 +1,52 @@
 import random
 def func():
     while True:
-       try:
-           x=int(input("How many questions do you want: "))
-           if x<=0:
-               raise ValueError
-           break
-       except ValueError:
-           print("Enter a numeric value greater than zero")
-
-    print("===Worksheet=== \n What comes in between ")
-    score=0
-    l2=[]
-    for i in range(1,x+1):
-        n=random.randint(11,100)
         while True:
-                try:
-                    print(f"{i}.) {n} __ {n+2}",end=" ")
+            print("\n \t\t\t\t===WHAT COMES IN BETWEEN===\n"
+                    "    \t\t\t\t         WORKSHEET   \n ")
+            try:
+                x=int(input("How many questions do you want: "))
+                if x<=0:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Enter a numeric value greater than zero")
 
-                    between=int(input(" ").strip())
-                    if between==n+1:
-                        score+=1
-                    
-                    else:
-                        l2.append(i)
-                    break
-             
-                except ValueError:
-                    print("Enter a numeric value")
-                    
-
-            
-    print(f"Score: {score}/{x}")
-    if score==x:
-        print("Excellent")
-    else:
-        if len(l2)==1:
-            print(f"Q{l2[0]} is wrong")
-        else:
     
-            print(", ".join("Q"+ str(i) for i in l2),end=" ")
-            print("are wrong")
+        score=0
+        wrong_ques=[]
+        for i in range(1,x+1):
+            n=random.randint(11,100)
+            while True:
+                    try:
+                        print(f"{i}.)   {n} __ {n+2}",end="  ")
+
+                        between=int(input(" ").strip())
+                        if between==n+1:
+                            score+=1
+                        
+                        else:
+                            wrong_ques.append(i)
+                        break
+                
+                    except ValueError:
+                        print("Enter a numeric value")
+                        
+
+                
+        print(f"\nScore: {score}/{x}")
+        if score==x:
+                print("Excellent")
+                print("All questions are correct 💯")
+        elif len(wrong_ques)==1:
+                print(f"Q{wrong_ques[0]} is wrong❌")
+        else:
+                print(", ".join("Q"+str(i) for i in wrong_ques),end=" ")
+                print("are wrong❌")
+        replay=input("\nReplay (yes/no)?: ").lower().strip()
+        if replay!="yes":   
+            print("Program ended")
+            break 
             
 
 if __name__=="__main__":
