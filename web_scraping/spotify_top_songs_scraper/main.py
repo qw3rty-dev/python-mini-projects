0d30269aa50207196524bc0c2805634f93caef1e
qw3rty-dev <rishabh120805@gmail.com>
2026-03-28 22:42:02 +0530

    Update main.py

--- web_scraping/spotify_top_songs_scraper/main.py
+++ web_scraping/spotify_top_songs_scraper/main.py
@@ -12,29 +12,30 @@ def song_scraper():
     req = requests.get(url, headers=headers)
     if req.status_code!=200:
         print("Request failed")
+    else:
 
-    soup=BeautifulSoup(req.text,"lxml")
-    table=soup.select("table.wikitable")
-    songs=table[0]
-    rows= songs.select("tr")
-    print(f"{'Rank':<8} {'Title':<50} {'Artist':<20} {'Streams (B)':<14} {'Release date'}")
-    print("-"*120)
-    with open("most_streamed_songs.csv", "w",newline="",encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(["Rank","Title","Artist","Streams(Billions)","Release Date"])
-        for rank,song in enumerate(rows):
-            title_tag=song.select_one("th a")
-            artist_tag=song.select_one("td a")
-            cells=song.select("td")
-            
+        soup=BeautifulSoup(req.text,"lxml")
+        table=soup.select("table.wikitable")
+        songs=table[0]
+        rows= songs.select("tr")
+        print(f"{'Rank':<8} {'Title':<50} {'Artist':<20} {'Streams (B)':<14} {'Release date'}")
+        print("-"*120)
+        with open("most_streamed_songs.csv", "w",newline="",encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Rank","Title","Artist","Streams(Billions)","Release Date"])
+            for rank,song in enumerate(rows):
+                title_tag=song.select_one("th a")
+                artist_tag=song.select_one("td a")
+                cells=song.select("td")
+                
 
-            if title_tag and artist_tag and len(cells)>=3:
-                title=title_tag.get("title")
-                artist=artist_tag.get("title")
-                streams=cells[-3].text.strip()
-                release_date=cells[-2].get_text()
-                writer.writerow([rank,title,artist,streams,release_date])
-                print(f"{rank:<8} {title:<50} {artist:<20} {streams} {'B':<8} {release_date}")
+                if title_tag and artist_tag and len(cells)>=3:
+                    title=title_tag.get("title")
+                    artist=artist_tag.get("title")
+                    streams=cells[-3].text.strip()
+                    release_date=cells[-2].get_text()
+                    writer.writerow([rank,title,artist,streams,release_date])
+                    print(f"{rank:<8} {title:<50} {artist:<20} {streams} {'B':<8} {release_date}")
 
 if __name__=="__main__":
-    song_scraper()
+     song_scraper()
